025e33bfeba49928311136d7d38eb55b08dc4389
dtinth on MBP M1 <dtinth@spacet.me>
2022-10-18 17:08:43 +0700

    No React plugin

--- packages/interaction-highlighter/vite.config.ts
+++ packages/interaction-highlighter/vite.config.ts
@@ -1,24 +1,13 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   build: {
     sourcemap: true,
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       formats: ['es'],
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
     },
   },
 })
