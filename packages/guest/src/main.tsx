fc9775ca2b6cc4bafa105f6dbd2764f4cced8f94
dtinth on MBP M1 <dtinth@spacet.me>
2022-10-18 16:56:36 +0700

    Use interaction-highlighter in tests

--- packages/guest/src/main.tsx
+++ packages/guest/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { DemoMiniApp } from './DemoMiniApp'
 import { MiniAppPage } from './packlets/guest'
+import { enableInteractionHighlighter } from '@eventpop-oss/interaction-highlighter'
 import './main.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
@@ -9,3 +10,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <MiniAppPage miniApp={DemoMiniApp} />
   </React.StrictMode>,
 )
+
+if (navigator.userAgent.includes('Playwright/')) {
+  enableInteractionHighlighter()
+}
