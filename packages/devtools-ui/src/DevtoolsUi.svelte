5ce0ecc8ffae3ff78386b705bc2de31f8768d453
dtinth on MBP M1 <dtinth@spacet.me>
2022-10-29 07:52:25 +0700

    Implement a command system

--- packages/devtools-ui/src/DevtoolsUi.svelte
+++ packages/devtools-ui/src/DevtoolsUi.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-  import { onMount } from 'svelte'
+  import { afterUpdate, onMount } from 'svelte'
   import { scale } from 'svelte/transition'
+  import type { Command } from './Command'
   let dragging:
     | {
         id: number
@@ -8,10 +9,17 @@
         startMouseY: number
         startButtonX: number
         startButtonY: number
+        moved: boolean
       }
     | undefined
+
   let button: HTMLElement
+  let menuElement: HTMLElement
   let mounted = false
+  let menu: { position?: { x: number; y: number } } | undefined
+  export let enabled = false
+  export let commands: Command[] = []
+
   onMount(() => {
     mounted = true
   })
@@ -25,6 +33,7 @@
   }
   let x = f('devtools-ui.x', 1)
   let y = f('devtools-ui.y', 0)
+  let disableClickBecauseJustMoved = false
 
   function handlePointerDown(e: PointerEvent) {
     dragging = {
@@ -33,14 +42,21 @@
       startMouseY: e.clientY,
       startButtonX: button.getBoundingClientRect().left,
       startButtonY: button.getBoundingClientRect().top,
+      moved: false,
     }
   }
   function handlePointerUp(e: PointerEvent) {
     if (!dragging || dragging.id !== e.pointerId) return
+    if (dragging.moved) {
+      e.preventDefault()
+      disableClickBecauseJustMoved = true
+      requestAnimationFrame(() => {
+        disableClickBecauseJustMoved = false
+      })
+    }
     dragging = undefined
     sessionStorage.setItem('devtools-ui.x', x.toString())
     sessionStorage.setItem('devtools-ui.y', y.toString())
-    e.preventDefault()
   }
   function handlePointerMove(e: PointerEvent) {
     if (!dragging || dragging.id !== e.pointerId) return
@@ -48,22 +64,96 @@
     const targetY = dragging.startButtonY + e.clientY - dragging.startMouseY
     x = Math.min(1, Math.max(0, targetX / (window.innerWidth - 64)))
     y = Math.min(1, Math.max(0, targetY / (window.innerHeight - 64)))
+    if (
+      !dragging.moved &&
+      Math.hypot(
+        e.clientX - dragging.startMouseX,
+        e.clientY - dragging.startMouseY,
+      ) > 3
+    ) {
+      dragging.moved = true
+      if (menu) {
+        menu = undefined
+      }
+    }
     e.preventDefault()
+  }
+  function handleClick() {
+    if (disableClickBecauseJustMoved) return
+    menu = menu ? undefined : {}
+  }
+  afterUpdate(() => {
+    if (menu && !menu.position && menuElement) {
+      const buttonRect = button.getBoundingClientRect()
+      const menuRect = menuElement.getBoundingClientRect()
+
+      const x =
+        buttonRect.left + menuRect.width > window.innerWidth &&
+        buttonRect.left + buttonRect.width - menuRect.width >= 0
+          ? buttonRect.left + buttonRect.width - menuRect.width
+          : buttonRect.left
+      const y =
+        buttonRect.top + buttonRect.height + menuRect.height >
+          window.innerHeight && buttonRect.top - menuRect.height >= 0
+          ? buttonRect.top - menuRect.height
+          : buttonRect.top + buttonRect.height
+      menu.position = { x, y }
+    }
+  })
+  function runCommand(command: Command) {
+    menu = undefined
+    command.action()
   }
 </script>
 
-{#if mounted}
-  <button
+{#if mounted && enabled && commands.length > 0}
+  <div
     transition:scale
-    class="button"
-    bind:this={button}
-    on:pointerdown={handlePointerDown}
-    style="left: {(x * 100).toFixed(2)}%; top: {(y * 100).toFixed(
-      2,
-    )}%; transform: translate({(x * -100).toFixed(2)}%, {(y * -100).toFixed(
-      2,
-    )}%)">🧑‍💻</button
+    style="
+      position: fixed;
+      left: {(x * 100).toFixed(2)}%;
+      top: {(y * 100).toFixed(2)}%;
+      transform-origin: 0 0;
+    "
   >
+    <button
+      class="button"
+      bind:this={button}
+      on:pointerdown={handlePointerDown}
+      data-testid="Developer tools button"
+      style="
+        transform: translate(
+          {(x * -100).toFixed(2)}%,
+          {(y * -100).toFixed(2)}%
+        );
+      "
+      on:click={handleClick}
+    >
+      🧑‍💻
+    </button>
+  </div>
+{/if}
+
+{#if mounted && enabled && commands.length > 0 && !!menu}
+  <div
+    class="menu"
+    bind:this={menuElement}
+    data-positioned={menu.position ? 'true' : 'false'}
+    style="
+      left: {menu.position?.x || 0}px;
+      top: {menu.position?.y || 0}px;
+    "
+  >
+    {#each commands as command (command.title)}
+      <button
+        class="menu-item"
+        on:click={() => runCommand(command)}
+        on:mouseenter={(e) => e.currentTarget.focus()}
+      >
+        {command.title}
+      </button>
+    {/each}
+  </div>
 {/if}
 
 <svelte:window
@@ -73,9 +163,7 @@
 
 <style>
   .button {
-    position: fixed;
-    top: 0;
-    left: 0;
+    display: block;
     width: 64px;
     height: 64px;
     border: none;
@@ -90,5 +178,32 @@
   }
   .button:hover {
     background: #00000020;
+  }
+  .menu {
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: flex;
+    flex-direction: row;
+    padding: 2px;
+    background: #d3d0c7;
+    box-shadow: inset -1px -1px 0 #424142, inset 1px 1px 0 #fff,
+      inset -2px -2px 0 #848284, inset 2px 2px 0 #d3d0c7;
+  }
+  .menu[data-positioned='false'] {
+    opacity: 0;
+    pointer-events: none;
+  }
+  .menu-item {
+    appearance: none;
+    border: none;
+    padding: 4px 8px;
+    background: transparent;
+    color: #000;
+    text-align: left;
+  }
+  .menu-item:focus {
+    background: #07216c;
+    color: #fff;
   }
 </style>
--- packages/devtools-ui/src/index.ts
+++ packages/devtools-ui/src/index.ts
@@ -1,13 +1,52 @@
+import { Command } from './Command'
 import DevtoolsUi from './DevtoolsUi.svelte'
 
 const _DevtoolsUi = DevtoolsUi as any
 customElements.define('devtools-ui', _DevtoolsUi)
 
-export class Devtools {
-  devtoolsUi: any
+export interface Disposable {
+  dispose(): void
+}
 
-  constructor() {
-    this.devtoolsUi = new _DevtoolsUi()
-    document.body.appendChild(this.devtoolsUi)
+export class Devtools {
+  private _devtoolsUi?: any
+  private _enabled = false
+  private _commands: Command[] = []
+
+  constructor() {}
+
+  get enabled() {
+    return this._enabled
+  }
+
+  set enabled(enabled: boolean) {
+    this._enabled = enabled
+    this._update()
+  }
+
+  _update() {
+    if (!this._enabled && !this._devtoolsUi) return
+    if (!this._devtoolsUi) {
+      this._devtoolsUi = new _DevtoolsUi()
+      document.body.appendChild(this._devtoolsUi)
+    }
+    this._devtoolsUi.$set({
+      enabled: this._enabled,
+      commands: this._commands,
+    })
+  }
+
+  registerCommand(title: string, action: () => void): Disposable {
+    const command: Command = { title, action }
+    this._commands = [...this._commands, command].sort((a, b) =>
+      a.title.localeCompare(b.title),
+    )
+    this._update()
+    return {
+      dispose: () => {
+        this._commands = this._commands.filter((c) => c !== command)
+        this._update()
+      },
+    }
   }
 }
